0927175a46bf404438bf9f3f6bca3d0ba7f59d8a
AlaaAl-Ghandour <alghandouraa@students.rhu.edu.lb>
2026-06-13 12:08:18 +0300

    Redeploy with updates

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   siteUrl: "https://tayibatai.com",
   generateRobotsTxt: true,
+  exclude: [
+    "/account",
+    "/admin",
+    "/admin/*",
+    "/chat",
+    "/payment/*",
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/account",
+          "/admin",
+          "/admin/",
+          "/chat",
+          "/payment/success",
+          "/payment/cancel",
+        ],
+      },
+    ],
+  },
 };
